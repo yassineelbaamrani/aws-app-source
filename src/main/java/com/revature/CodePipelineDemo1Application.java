a976509547059b2f183956000dcaa3c6ee1debb9
Yassine Elbaamrani <yelbaamrani@hotmail.com>
2021-09-17 11:05:32 -0400

    add buildspec.yml and exploded war plugin

---
+++ src/main/java/com/revature/CodePipelineDemo1Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePipelineDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePipelineDemo1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/revature/HelloController.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+	
+		return "What's up!";
+	}
+
+}
